817b9b25494e05f7d8b92db06566e0edc8057509
Christopher Baker <csb048@usask.ca>
2023-06-19 17:40:06 +0000

    set PROJECT_DIT to to point to the Summa-openWQ directory

--- utils/containers/summa_openwq/launch_docker_container.sh
+++ utils/containers/summa_openwq/launch_docker_container.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export PROJECT_DIR=/Users/kyleklenk/SUMMA-Projects/Summa-openWQ
+export PROJECT_DIR=/home/cbaker/Hydro/Summa-openWQ
 
 docker run -d -it --name SUMMA-openWQ --mount type=bind,source=${PROJECT_DIR},target=/code/Summa-OpenWQ \
     summa-openwq:latest
