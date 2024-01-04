f042e4ef443c17f7cbd361fcdf4c4ab0cd5e1218
Christopher Baker <csb048@usask.ca>
2024-01-04 17:29:02 -0600

    now using updated Dockerfile, added attional mapping for a data directory

--- utils/containers/summa_openwq/launch_docker_container.sh
+++ utils/containers/summa_openwq/launch_docker_container.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-export PROJECT_DIR=/Users/kyleklenk/SUMMA-Projects/Summa-openWQ
+export PROJECT_DIR=/home/cbaker/Hydro/Summa-openWQ-revised-install
+export DATA_DIR=/home/cbaker/Hydro/SUMMAapptainer/case_studies/mizuroute_Great_Slave_Lake
 
-docker run -d -it --name SUMMA-openWQ --mount type=bind,source=${PROJECT_DIR},target=/code/Summa-OpenWQ \
+docker run -d -it --name SUMMA-openWQ \
+    --mount type=bind,source=${PROJECT_DIR},target=/code/Summa-OpenWQ \
+    --mount type=bind,source=${DATA_DIR},target=/code/data \
     summa-openwq:latest
